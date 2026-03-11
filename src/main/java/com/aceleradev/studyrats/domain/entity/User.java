dae7a909375f81dd0fc0537190ef1d6c8c9660d6
Luanderson Mendes <contato@luanderson.dev.br>
2026-03-10 21:25:40 -0400

    feat(domain): add User entity

---
+++ src/main/java/com/aceleradev/studyrats/domain/entity/User.java
@@ -0,0 +1,35 @@
+package com.aceleradev.studyrats.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = LocalDateTime.now();
+    }
+}
