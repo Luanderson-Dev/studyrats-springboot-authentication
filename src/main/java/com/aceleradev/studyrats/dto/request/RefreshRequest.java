d51081737710e51a97dfd79d7eced3b5b28337b2
Luanderson Mendes <contato@luanderson.dev.br>
2026-03-10 21:31:10 -0400

    feat(dto): add request and response DTOs.

---
+++ src/main/java/com/aceleradev/studyrats/dto/request/RefreshRequest.java
@@ -0,0 +1,8 @@
+package com.aceleradev.studyrats.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RefreshRequest {
+    private String refreshToken;
+}
---
+++ src/main/java/com/aceleradev/studyrats/dto/response/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.aceleradev.studyrats.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+}
