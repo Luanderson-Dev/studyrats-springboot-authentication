097111c9643166eeacf57d19889d99b0a15a1989
Luanderson Mendes <contato@luanderson.dev.br>
2026-03-10 21:29:17 -0400

    feat(config): add JwtService for token validation and generation.

---
+++ src/main/java/com/aceleradev/studyrats/config/JwtService.java
@@ -0,0 +1,62 @@
+package com.aceleradev.studyrats.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String userId) {
+
+        Date now = new Date();
+
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .subject(userId)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUserId(String token) {
+
+        Claims claims = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith( getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (SignatureException e) {
+            return false;
+        }
+    }
+}
